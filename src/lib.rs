6dd9494cdc8e3ce1337d23d108294707e85ae40c
Chet Murthy <chetsky@gmail.com>
2022-06-11 12:23:07 -0700

    meh

--- src/lib.rs
+++ src/lib.rs
@@ -19,42 +19,46 @@ impl PauliKind {
             'X' => Ok(PauliKind::X),
             'Y' => Ok(PauliKind::Y),
             'Z' =>  Ok(PauliKind::Z),
-            _ => Err(String::from("internal error: malformed paulis"))
+            _ => Err(String::from("internal error: malformed pauli"))
         }
     }
 }
 
-pub struct PauliData {
+pub struct Pauli {
     coeff : Complex64,
     paulis : Vec<PauliKind>,
 }
-impl PauliData {    
+impl Pauli {    
 
-pub fn parse_label(s : &str) -> PauliData {
-    lazy_static! {
-        static ref RE : Regex = Regex::new(r"^([+-]?)1?([ij]?)([IXYZ]+)$").unwrap();
+    fn parse_label(s : &str) -> Result<(Complex64, Vec<PauliKind>),  String> {
+        lazy_static! {
+            static ref RE : Regex = Regex::new(r"^([+-]?)1?([ij]?)([IXYZ]+)$").unwrap();
+        }
+        let caps = RE.captures(s).unwrap();
+
+        let sign = caps.get(1).map_or("", |m| m.as_str());
+        let imag = caps.get(2).map_or("", |m| m.as_str());
+        let paulistr = caps.get(3).map_or("", |m| m.as_str());
+
+        let sign = match sign { ""|"+" => Ok(true), "-" => Ok(false), _ => Err("internal error: malformed sign") } ?;
+        let coeff = match imag {
+            "" => Ok(Complex64::new(1.0,  0.0)),
+            "i"|"j" => Ok(Complex64::new(0.0, 1.0)),
+            _ => Err("internal error: malformed imag")
+        }?;
+        let coeff = if sign { coeff } else { - coeff } ;
+        
+        let mut paulis = Vec::new() ;
+        for c in paulistr.chars() {
+            paulis.push(PauliKind::new(c)?)
+        }
+        if 0 == paulis.len() { Err(String::from("internal error: no paulis")) }
+        else { Ok((coeff, paulis)) }
     }
-    let caps = RE.captures(s).unwrap();
 
-    let sign = caps.get(1).map_or("", |m| m.as_str());
-    let imag = caps.get(2).map_or("", |m| m.as_str());
-    let paulis = caps.get(3).map_or("", |m| m.as_str());
-
-    let sign = match sign { ""|"+" => true, "-" => false, _ => panic!("internal error: malformed sign") } ;
-    let coeff = match imag {
-        "" => Complex64::new(1.0,  0.0),
-        "i"|"j" => Complex64::new(0.0, 1.0),
-        _ => panic!("internal error: malformed imag")
-    };
-    let coeff = if sign { coeff } else { - coeff } ;
-            
-    let mut paulis = Vec::new() ;
-    for c in s.chars() {
-        paulis.push(match PauliKind::new(c) {
-            Ok(v) => v,
-            _ => panic!("internal error: malformed paulis")
-        })
+    pub fn new(s : &str) -> Result<Pauli, String> {
+        let (coeff, paulis) = Pauli::parse_label(s)? ;
+        Ok(Pauli{ coeff, paulis })
     }
-    PauliData{ coeff, paulis }
-}
+
 }
