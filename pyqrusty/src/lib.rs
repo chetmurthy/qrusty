a6d0dcb337cec75d579260a16042ad706da9d6e4
Chet Murthy <chetsky@gmail.com>
2022-06-18 23:10:58 -0700

    start with python wrappering of Rust structs

---
+++ pyqrusty/src/lib.rs
@@ -0,0 +1,37 @@
+
+use pyo3::prelude::*;
+
+#[pyclass]
+pub struct RustStruct {
+    #[pyo3(get, set)]
+    pub data: String,
+    #[pyo3(get, set)]
+    pub vector: Vec<u8>,
+}
+
+#[pymethods]
+impl RustStruct {
+    #[new]
+    pub fn new(data: String, vector: Vec<u8>) -> RustStruct {
+        RustStruct { data, vector }
+    }
+    pub fn printer(&self) {
+        println!("{}", self.data);
+        for i in &self.vector {
+            println!("{}", i);
+        }
+    }
+    pub fn extend_vector(&mut self, extension: Vec<u8>) {
+        println!("{}", self.data);
+        for i in extension {
+            self.vector.push(i);
+        }
+    }
+}
+
+/// A Python module implemented in Rust.
+#[pymodule]
+fn pyqrusty(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_class::<RustStruct>()?;
+    Ok(())
+}
