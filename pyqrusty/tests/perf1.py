d5963ca0acff4283e2f7365d1414759fdfbfc6f5
Chet Murthy <Chetan.Murthy@ibm.com>
2022-07-01 01:07:20 -0500

    meh

--- pyqrusty/tests/perf1.py
+++ pyqrusty/tests/perf1.py
@@ -15,4 +15,14 @@ def timer(msg, f):
     print('END ELAPSED %s: %.03fms' % (msg, 1000 * (t1-t0)))
     return rv
 
-timer("foo", lambda: H8.to_matrix_mode("RowwiseUnsafeChunked/100"))
+timer("H2+RowwiseUnsafeChunked/100", lambda: H2.to_matrix_mode("RowwiseUnsafeChunked/100"))
+timer("H4+RowwiseUnsafeChunked/100", lambda: H4.to_matrix_mode("RowwiseUnsafeChunked/100"))
+timer("H6+RowwiseUnsafeChunked/100", lambda: H6.to_matrix_mode("RowwiseUnsafeChunked/100"))
+timer("H8+RayonChunked/100", lambda: H8.to_matrix_mode("RayonChunked/100"))
+timer("H8+RowwiseUnsafeChunked/100", lambda: H8.to_matrix_mode("RowwiseUnsafeChunked/100"))
+timer("H10+RayonChunked/1000", lambda: H10.to_matrix_mode("RayonChunked/1000"))
+timer("H10+RowwiseUnsafeChunked/1000", lambda: H10.to_matrix_mode("RowwiseUnsafeChunked/1000"))
+timer("H11+RayonChunked/1000", lambda: H11.to_matrix_mode("RayonChunked/1000"))
+timer("H11+RowwiseUnsafeChunked/1000", lambda: H11.to_matrix_mode("RowwiseUnsafeChunked/1000"))
+timer("H12+RayonChunked/1000", lambda: H12.to_matrix_mode("RayonChunked/1000"))
+timer("H12+RowwiseUnsafeChunked/1000", lambda: H12.to_matrix_mode("RowwiseUnsafeChunked/1000"))
