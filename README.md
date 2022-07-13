600a512d8d73ffc1e28c5a386802520a017f028e
Chet Murthy <chetsky@gmail.com>
2022-07-13 16:09:15 -0700

    added copyrights

---
+++ README.md
@@ -0,0 +1,14 @@
+# qrusty
+[![License](https://img.shields.io/github/license/Qiskit/qiskit-terra.svg?style=popout-square)](https://opensource.org/licenses/Apache-2.0)
+
+**Qrusty** is a small library for working with quantum-computing in Rust and Python.
+
+It is meant as a sandbox to experiment with code that is meant to be compatible with IBM's Qiskit.
+
+## Installation
+
+Installation instructions here.
+
+## License
+
+[Apache License 2.0](LICENSE.txt)
--- qrusty/src/fixtures.rs
+++ qrusty/src/fixtures.rs
@@ -1,3 +1,13 @@
+// (C) Copyright IBM 2022
+//
+// This code is licensed under the Apache License, Version 2.0. You may
+// obtain a copy of this license in the LICENSE.txt file in the root directory
+// of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+//
+// Any modifications or derivative works of this code must retain this
+// copyright notice, and modified files need to carry a notice indicating
+// that they have been altered from the originals.
+
 #![allow(non_upper_case_globals)]
 use num_complex::Complex64;
 use crate::util ;
